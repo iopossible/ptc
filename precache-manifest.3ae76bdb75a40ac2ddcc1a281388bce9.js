cf788de7adad4f5fe74ce6bdf0ff447126de2d6d
IO Possible <iopossible@IOs-iMac.local>
2020-02-22 09:48:35 -0700

    Initial commit

---
+++ precache-manifest.3ae76bdb75a40ac2ddcc1a281388bce9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee4a093c96f4b786b56240710acf639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d109d5728499c13b280",
+    "url": "/static/css/main.b11a34e7.chunk.css"
+  },
+  {
+    "revision": "9656fd84f4328b324a80",
+    "url": "/static/js/2.8462d3b0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8462d3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d109d5728499c13b280",
+    "url": "/static/js/main.857055a7.chunk.js"
+  },
+  {
+    "revision": "15fbf8aa72ef38551952",
+    "url": "/static/js/runtime-main.26f28ca5.js"
+  },
+  {
+    "revision": "82b5db1c95313dfa3e9e2263389a0c87",
+    "url": "/static/media/batteries.82b5db1c.svg"
+  },
+  {
+    "revision": "a2995f53bb6029a561730f655331e0c2",
+    "url": "/static/media/logo.a2995f53.svg"
+  },
+  {
+    "revision": "69c324f4e386c7f4336399b84716aea4",
+    "url": "/static/media/paper_towel.69c324f4.svg"
+  },
+  {
+    "revision": "37f16cccfeb5c51ed7146b5b907ed6c1",
+    "url": "/static/media/razor.37f16ccc.svg"
+  },
+  {
+    "revision": "8b6e135ff2bf1d9f8de5cc01db5b8d80",
+    "url": "/static/media/soap.8b6e135f.svg"
+  },
+  {
+    "revision": "e7e716060d71c530d25511a4d9a984e8",
+    "url": "/static/media/socks.e7e71606.svg"
+  },
+  {
+    "revision": "a4bfccc4e687979d51565c1c62752fb5",
+    "url": "/static/media/toilet_paper.a4bfccc4.svg"
+  },
+  {
+    "revision": "acc4adb195c2848cb003cf0db0efd384",
+    "url": "/static/media/tshirt.acc4adb1.svg"
+  },
+  {
+    "revision": "3702f064b97ee6b41a48a9ee57c0da41",
+    "url": "/static/media/underwear.3702f064.svg"
+  }
+]);
